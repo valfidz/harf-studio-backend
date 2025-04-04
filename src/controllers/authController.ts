c2928cfeffb8dd0092e21a8f5aff724495ea2877
ayiakbarmaulana <opinigpt@gmail.com>
2025-04-04 16:11:00 +0700

    add: verifyTokenCookies, and save token to cookies httponly

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,180 +1,211 @@
-import { Request, Response } from 'express';
-import sql from '../config/database';
+import { Request, Response } from "express";
+import sql from "../config/database";
 import bcrypt, { compareSync } from "bcryptjs";
-import { generateJWToken, verifyToken } from '../utils/jwt';
-import { redis } from '../config/redis';
-import { Session } from '../types/token';
-import { encryptKey } from '../utils/encrypt';
+import { generateJWToken, verifyToken } from "../utils/jwt";
+import { redis } from "../config/redis";
+import { Session } from "../types/token";
+import { encryptKey } from "../utils/encrypt";
+import jwt from "jsonwebtoken";
 
 export const userRegister = async (req: Request, res: Response): Promise<any> => {
-    try {
+  try {
+    // get and checking req.body value
+    const { name, company_name, email, password } = req.body;
+    const role = "member";
 
-        // get and checking req.body value
-        const { name, company_name, email, password } = req.body;
-        const role = "member";
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        error: "Input parameter is missing",
+      });
+    }
 
-        if (!name || !email || !password) {
-            return res.status(400).json({
-                error: 'Input parameter is missing'
-            })
-        }
+    // create hash password
+    const salt = bcrypt.genSaltSync(10);
+    const hashPassword = bcrypt.hashSync(password, salt);
 
-        // create hash password
-        const salt = bcrypt.genSaltSync(10);
-        const hashPassword = bcrypt.hashSync(password, salt);
-
-        const data = await sql`
+    const data = await sql`
             INSERT INTO users (name, company_name, email, password, role)
             VALUES (${name}, ${company_name}, ${email}, ${hashPassword}, ${role})
             RETURNING id, name, company_name, email, role
-        `
+        `;
 
-        // token generation
-        const userData = {
-            id: data[0].id,
-            name: data[0].name,
-            company_name: data[0].company_name,
-            email: data[0].email,
-            role: data[0].role
-        }
+    // token generation
+    const userData = {
+      id: data[0].id,
+      name: data[0].name,
+      company_name: data[0].company_name,
+      email: data[0].email,
+      role: data[0].role,
+    };
 
-        // encrypt email for redis key
-        const encryptMail = await encryptKey(email);
+    // encrypt email for redis key
+    const encryptMail = await encryptKey(email);
 
-        const token = generateJWToken(userData);
+    const token = generateJWToken(userData);
 
-        if (typeof token === "object" && ("errorCode" in token) && (token.errorCode === "13" || token.errorCode === "14")) {
-            return res.status(400).json({
-                errorCode: token.errorCode,
-                message: token.message
-            })
-        }
-
-        res.setHeader('Authorization', `Bearer ${token}`);
-        await redis.set(`user_session:${encryptMail}`, { token: token }, { ex: 86400 });
-
-        // return result
-        return res.status(201).json({
-            message: `User ${data[0].name} registered successfully!`,
-            user_data: data[0]
-        })
-    } catch (error: any) {
-        return res.status(500).json({
-            error: error.message
-        })
+    if (
+      typeof token === "object" &&
+      "errorCode" in token &&
+      (token.errorCode === "13" || token.errorCode === "14")
+    ) {
+      return res.status(400).json({
+        errorCode: token.errorCode,
+        message: token.message,
+      });
     }
-}
+
+    res.setHeader("Authorization", `Bearer ${token}`);
+    await redis.set(`user_session:${encryptMail}`, { token: token }, { ex: 86400 });
+
+    // return result
+    return res.status(201).json({
+      message: `User ${data[0].name} registered successfully!`,
+      user_data: data[0],
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+};
 
 export const userLogin = async (req: Request, res: Response): Promise<any> => {
-    try {
-        // get and check req.body value
-        const { email, password } = req.body;
+  try {
+    // get and check req.body value
+    const { email, password } = req.body;
 
-        if (!email || !password) {
-            return res.status(400).json({
-                error: 'Credentials is missing!'
-            })
-        }
+    if (!email || !password) {
+      return res.status(400).json({
+        error: "Credentials is missing!",
+      });
+    }
 
-        // encrypt email for redis key
-        const encryptMail = await encryptKey(email);
+    // encrypt email for redis key
+    const encryptMail = await encryptKey(email);
 
-        // check session on redis
-        const session = await redis.get<Session>(`user_session:${encryptMail}`);
-        let token;
+    // check session on redis
+    const session = await redis.get<Session>(`user_session:${encryptMail}`);
+    let token;
 
-        if (!session) {
-            // get user data from database
-            const user = await sql`
+    if (!session) {
+      // get user data from database
+      const user = await sql`
                 SELECT id, name, company_name, email, role, password
                 FROM users
                 WHERE email = ${email} 
                 AND deleted_at IS NULL
-            `
+            `;
 
-            if (!user) {
-                return res.status(404).json({
-                    error: 'User not found!'
-                })
-            }
+      if (!user) {
+        return res.status(404).json({
+          error: "User not found!",
+        });
+      }
 
-            // compare password
-            const userData = {
-                id: user[0].id,
-                name: user[0].name,
-                company_name: user[0].company_name,
-                email: user[0].email,
-                role: user[0].role
-            }
+      // compare password
+      const userData = {
+        id: user[0].id,
+        name: user[0].name,
+        company_name: user[0].company_name,
+        email: user[0].email,
+        role: user[0].role,
+      };
 
-            const isMatch = await bcrypt.compare(password, user[0]?.password)
+      const isMatch = await bcrypt.compare(password, user[0]?.password);
 
-            if (!isMatch) {
-                return res.status(400).json({
-                    error: 'Invalid credentials!'
-                })
-            }
+      if (!isMatch) {
+        return res.status(400).json({
+          error: "Invalid credentials!",
+        });
+      }
 
-            // token generation
-            const generateToken = generateJWToken(userData);
-            await redis.set(`user_session:${encryptMail}`, { token: generateToken }, { ex: 86400 });
+      // token generation
+      const generateToken = generateJWToken(userData);
+      await redis.set(`user_session:${encryptMail}`, { token: generateToken }, { ex: 86400 });
 
-            if (typeof generateToken === "object" && ("errorCode" in generateToken) && (generateToken.errorCode === "13" || generateToken.errorCode === "14")) {
-                return res.status(400).json({
-                    errorCode: generateToken.errorCode,
-                    message: generateToken.message
-                })
-            }
+      if (
+        typeof generateToken === "object" &&
+        "errorCode" in generateToken &&
+        (generateToken.errorCode === "13" || generateToken.errorCode === "14")
+      ) {
+        return res.status(400).json({
+          errorCode: generateToken.errorCode,
+          message: generateToken.message,
+        });
+      }
 
-            token = generateToken;
-        } else {
-            token = session.token;
-        }
-
-       
-        res.setHeader('Authorization', `Bearer ${token}`);
-
-        // return result
-        return res.status(200).json({
-            message: 'Login successful!',
-            token
-        })
-    } catch (error: any) {
-        return res.status(500).json({
-            error: error.message
-        })
+      token = generateToken;
+    } else {
+      token = session.token;
     }
-}
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    res.setHeader("Authorization", `Bearer ${token}`);
+
+    // return result
+    return res.status(200).json({
+      message: "Login successful!",
+      token,
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+};
 
 export const authVerify = async (req: Request, res: Response): Promise<any> => {
-    try {
-        const { token } = req.body;
+  try {
+    const { token } = req.body;
 
-        if (!token) {
-            return res.status(400).json({
-                message: 'Token is missing'
-            })
-        }
-
-        const validate = verifyToken(token)
-
-        if (!validate.valid) {
-            return res.status(500).json({
-                valid: validate.valid,
-                error: validate.error
-            })
-        }
-
-        return res.status(200).json({
-            valid: validate.valid,
-            name: validate.decoded?.name,
-            company_name: validate.decoded?.company_name,
-            email: validate.decoded?.email,
-            role: validate.decoded?.role,
-        })
-    } catch (error: any) {
-        return res.status(500).json({
-            error: error.message
-        })
+    if (!token) {
+      return res.status(400).json({
+        message: "Token is missing",
+      });
     }
-}
+
+    const validate = verifyToken(token);
+
+    if (!validate.valid) {
+      return res.status(500).json({
+        valid: validate.valid,
+        error: validate.error,
+      });
+    }
+
+    return res.status(200).json({
+      valid: validate.valid,
+      name: validate.decoded?.name,
+      company_name: validate.decoded?.company_name,
+      email: validate.decoded?.email,
+      role: validate.decoded?.role,
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+
+export const verifyTokenCookies = async (req: Request, res: Response): Promise<any> => {
+  const token = req.cookies.token;
+  if (!token) return res.status(401).json({ message: "Not authenticated!" });
+
+  try {
+    const decoded = jwt.verify(token, String(process.env.JWT_SECRET));
+    res.json({ user: decoded });
+  } catch (error) {
+    if (error instanceof Error) {
+      if (error.name === "TokenExpiredError") {
+        return res.status(401).json({ message: "Token expired, please login again" });
+      }
+    }
+    res.status(403).json({ message: "Invalid token" });
+  }
+};
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,12 +1,18 @@
-import express, { Router } from 'express';
-import { userRegister, userLogin, authVerify } from '../controllers/authController';
-import { validate } from '../utils/zod';
-import { registerSchema, loginSchema } from '../helpers/validation';
+import express, { Router } from "express";
+import {
+  userRegister,
+  userLogin,
+  authVerify,
+  verifyTokenCookies,
+} from "../controllers/authController";
+import { validate } from "../utils/zod";
+import { registerSchema, loginSchema } from "../helpers/validation";
 
 const authRouter: Router = express.Router();
 
-authRouter.post('/register', validate(registerSchema), userRegister);
-authRouter.post('/login', validate(loginSchema), userLogin);
-authRouter.post('/verify', authVerify);
+authRouter.post("/register", validate(registerSchema), userRegister);
+authRouter.post("/login", validate(loginSchema), userLogin);
+authRouter.get("/verifyTokenCookies", verifyTokenCookies);
+authRouter.post("/verify", authVerify);
 
-export default authRouter
+export default authRouter;
