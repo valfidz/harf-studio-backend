4e646a22452596bcec7ad3eae797597e2c52d25f
ayiakbarmaulana <opinigpt@gmail.com>
2025-04-04 16:03:58 +0700

    build: add cookie parser to package.json

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,21 @@
-import express from 'express';
-import cors from 'cors';
-import authRouter from './routes/authRoutes';
-import secureRouter from './routes/encryptionRoutes';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/authRoutes";
+import secureRouter from "./routes/encryptionRoutes";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
+app.use(cookieParser());
 app.use(express.json());
 
-app.use('/auth', authRouter);
-app.use('/secure', secureRouter);
+app.use("/auth", authRouter);
+app.use("/secure", secureRouter);
 
 export default app;
